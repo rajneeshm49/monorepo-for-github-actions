e21ca98c017a8fd4d29adf2f799979c242913a6a
Rajneesh <rajneesh.m49@gmail.com>
2023-05-19 17:00:16 -0500

    optimized caching further

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,17 @@
+import { notice, getInput, setOutput } from '@actions/core';
+import * as gh_actions from '@actions/github';
+import { exec } from '@actions/exec';
+
+run();
+
+function run() {
+  const bucket = getInput('bucket');
+  const region = getInput('region');
+  const distFolder = getInput('dist-folder');
+
+  notice('Hello from github action');
+  exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${region}`);
+
+  const s3URL = `https://${bucket}.s3-website-${region}.amazonaws.com`;
+  setOutput('s3-url', s3URL);
+}
