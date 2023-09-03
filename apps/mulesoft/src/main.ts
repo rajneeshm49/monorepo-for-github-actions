b7a5e488c6e3192f7d1b63fc1a70b2580a0aacd4
Rajneesh <rajneesh.m49@gmail.com>
2023-09-03 07:57:20 -0500

    add fe part

--- apps/mulesoft/src/main.ts
+++ apps/mulesoft/src/main.ts
@@ -7,10 +7,16 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+  app.enableCors({
+    origin: environment.FE_URL,
+    preflightContinue: true,
+    methods: 'GET, POST',
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}`);
