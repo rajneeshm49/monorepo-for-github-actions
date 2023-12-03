d2b2d753333e275741bc7d546177ad7c046be12c
Rajneesh <rajneesh.m49@gmail.com>
2023-12-03 05:44:43 -0500

    add dynamic api url in gh

--- infra/create-eks-cluster.sh
+++ infra/create-eks-cluster.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-eksctl create cluster --name Kubernetes-demo --region us-east-1 --node-type t3.micro --nodes-min 2 --nodes-max 2
+eksctl create cluster --name Kubernetes-demo --region us-east-1 --node-type t3.medium --nodes-min 2 --nodes-max 2
